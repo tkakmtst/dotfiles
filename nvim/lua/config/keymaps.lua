ee4e4fb01d317826941e0229266ea50c09a4bb55
Takaaki Matsushita <takaaki.matsushita@medley.jp>
2026-02-15 00:03:06 +0900

    Add nvim keymaps for file path copy and auto-reload settings

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- ファイルパスをクリップボードにコピー
+vim.keymap.set("n", "<leader>yp", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied: " .. path, vim.log.levels.INFO)
+end, { desc = "Copy absolute file path" })
+
+vim.keymap.set("n", "<leader>yr", function()
+  local path = vim.fn.expand("%")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied: " .. path, vim.log.levels.INFO)
+end, { desc = "Copy relative file path" })
+
+-- ファイルを手動でリロード
+vim.keymap.set("n", "<leader>r", ":checktime<CR>", { desc = "Reload file from disk" })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,27 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- 行番号設定
+vim.opt.relativenumber = false  -- 相対行番号を無効化
+vim.opt.number = true            -- 絶対行番号を表示
+
+-- ファイルが外部で変更された場合に自動リロード
+vim.opt.autoread = true
+
+-- フォーカス時やバッファ変更時に自動チェック
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  command = "if mode() != 'c' | checktime | endif",
+})
+
+-- ファイル変更時に通知
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+  pattern = "*",
+  callback = function()
+    vim.notify("File changed on disk. Buffer reloaded.", vim.log.levels.WARN)
+  end,
+})
+
+-- クリップボード統合
+vim.opt.clipboard = "unnamedplus"
