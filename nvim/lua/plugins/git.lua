125c001300a1e66806dc9223c15e60bc8be5b7d6
Takaaki Matsushita <takaaki.matsushita@medley.jp>
2026-05-27 09:53:32 +0900

    Add git-conflict.nvim and disable ghostty font ligatures

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -26,4 +26,29 @@ return {
       { "<leader>gV", "<cmd>DiffviewClose<cr>",         desc = "Diff View Close" },
     },
   },
+
+  {
+    "akinsho/git-conflict.nvim",
+    version = "*",
+    event = "BufReadPre",
+    opts = {
+      default_mappings = true,
+      default_commands = true,
+      disable_diagnostics = false,
+      list_opener = "copen",
+      highlights = {
+        incoming = "DiffAdd",
+        current = "DiffText",
+      },
+    },
+    keys = {
+      { "<leader>gco", "<cmd>GitConflictChooseOurs<cr>",    desc = "Conflict: Choose Ours" },
+      { "<leader>gct", "<cmd>GitConflictChooseTheirs<cr>",  desc = "Conflict: Choose Theirs" },
+      { "<leader>gcb", "<cmd>GitConflictChooseBoth<cr>",    desc = "Conflict: Choose Both" },
+      { "<leader>gc0", "<cmd>GitConflictChooseNone<cr>",    desc = "Conflict: Choose None" },
+      { "<leader>gcl", "<cmd>GitConflictListQf<cr>",        desc = "Conflict: List in Quickfix" },
+      { "]x",          "<cmd>GitConflictNextConflict<cr>",  desc = "Next Conflict" },
+      { "[x",          "<cmd>GitConflictPrevConflict<cr>",  desc = "Prev Conflict" },
+    },
+  },
 }
